8709c2d9331427725b2c5e28d62d10aa7b9afff5
Karol Zadora-Przylecki <karolz@microsoft.com>
2016-02-11 11:13:05 -0800

    Preserve work

---
+++ src/Microsoft.Extensions.Logging.EventSource/EventSourceLoggerEventSource.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics.Tracing;
+
+namespace Microsoft.Extensions.Logging.EventSource.Internal
+{
+#if NET451
+    [EventSource(Name ="")]
+    public sealed class EventSourceLoggerEventSource
+    {
+
+    }
+#endif
+}
